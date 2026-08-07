392466b1b96dfb7550903982e0e81f3ba6fa39a0
bsha <benjamin.y.sha@gmail.com>
2026-08-07 14:06:52 -0400

    Simple mobile accommodation

--- web/components/wordle/Keyboard/Key/Key.tsx
+++ web/components/wordle/Keyboard/Key/Key.tsx
@@ -1,19 +1,22 @@
+import { Delete } from "lucide-react";
 import styles from "./Key.module.scss";
 import { Button } from "@/components/ui/button";
 
 type Props = { value: string; status: LetterState };
 
 const Key = ({ value, status }: Props) => {
-  const ariaLabel = value === "Enter" ? "Enter guess" : value === "Delete" ? "Delete letter" : `Letter ${value}`;
+  const isEnter = value === "Enter";
+  const isDelete = value === "Delete";
+  const ariaLabel = isEnter ? "Enter guess" : isDelete ? "Delete letter" : `Letter ${value}`;
 
   return (
     <Button
       variant="outline"
       type="button"
       aria-label={ariaLabel}
-      className={`p-3 m-0.5 uppercase ${styles["keyboard-key"]} ${styles[`${status}`]}`}
+      className={`p-2 m-0.5 uppercase ${styles["keyboard-key"]} ${styles[`${status}`]} lg:p-3`}
     >
-      {value}
+      {isDelete ? <Delete /> : value}
     </Button>
   );
 };
